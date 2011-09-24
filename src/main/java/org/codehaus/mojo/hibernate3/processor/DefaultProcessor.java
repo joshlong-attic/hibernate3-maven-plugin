54c9af2b25c2a76f57e6c26801f736d6ddce274e
Josh Long <josh.long@springsource.com>
2011-09-24 01:14:41 -0700

    added as2 license..

--- src/main/java/org/codehaus/mojo/hibernate3/processor/DefaultProcessor.java
+++ src/main/java/org/codehaus/mojo/hibernate3/processor/DefaultProcessor.java
@@ -1,3 +1,20 @@
+
+/*
+ *
+ * Copyright 2011 Josh Long
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.codehaus.mojo.hibernate3.processor;
 
 import org.codehaus.mojo.hibernate3.processor.implementations.*;
@@ -12,22 +29,20 @@ import java.util.List;
 public class DefaultProcessor implements CompositeProcessor {
     private List<GeneratedClassProcessor> processors;
 
-    private Class[] processorsArr = {
-                                            Asl2LicenseProcessor.class,
-                                            RequiredImportProcessor.class,
-                                            DateCreatedProcessor.class,
-                                            DateModifiedProcessor.class,
-
-                                            VersionProcessor.class,
-                                            GeneratedValueIdProcessor.class,
-                                            NonPrimitiveIdProcessor.class,
-                                            CacheProcessor.class
-    };
-
     public DefaultProcessor() throws Throwable {
         List<String> clzzNames = new ArrayList<String>();
 
-        for (Class c : this.processorsArr) {
+        Class[] processorsArr = {
+                                        Asl2LicenseProcessor.class,
+                                        RequiredImportProcessor.class,
+                                        DateCreatedProcessor.class,
+                                        DateModifiedProcessor.class,
+                                        VersionProcessor.class,
+                                        GeneratedValueIdProcessor.class,
+                                        NonPrimitiveIdProcessor.class,
+                                        CacheProcessor.class
+        };
+        for (Class c : processorsArr) {
             clzzNames.add(c.getName());
         }
 
--- src/main/java/org/codehaus/mojo/hibernate3/processor/implementations/AuditingProcessor.java
+++ src/main/java/org/codehaus/mojo/hibernate3/processor/implementations/AuditingProcessor.java
@@ -1,3 +1,20 @@
+/*
+*
+* Copyright 2011 Josh Long
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.codehaus.mojo.hibernate3.processor.implementations;
 
 import org.codehaus.mojo.hibernate3.processor.ComponentPropertiesAware;
@@ -8,19 +25,20 @@ import java.io.File;
 import java.util.Map;
 
 /**
- * Registers the {@link }
+ * Registers the an {@link javax.persistence.EntityListeners} by consulting the configured property for the Maven
+ * plugin named "auditEntityListenerClass".
  *
  * @author Josh Long
  */
-public class AuditingProcessor implements GeneratedClassProcessor , ComponentPropertiesAware{
+public class AuditingProcessor implements GeneratedClassProcessor, ComponentPropertiesAware {
 
-    static public final String AUDIT_ENTITY_LISTENER_CLASS= "auditEntityListenerClass" ;
+    static public final String AUDIT_ENTITY_LISTENER_CLASS = "auditEntityListenerClass";
 
     private String auditListenerClass;
 
     @Override
     public String processClass(File fi, String contents) {
-        return contents.replace("@Entity", String.format("@Entity @EntityListeners(%s.class)",  this.auditListenerClass));
+        return contents.replace("@Entity", String.format("@Entity @EntityListeners(%s.class)", this.auditListenerClass));
     }
 
     @Override

25fdede8cab310c3d990b7e56cb442edccb313ae
Josh Long <josh.long@springsource.com>
2011-09-24 03:53:24 -0700

    updating...

--- release.sh
+++ release.sh
@@ -1,5 +1,9 @@
 #first prepare the release 
 mvn release:prepare 
 
+tag=`cat release.properties |grep scm.tag|cut -f2 -d=|head -n1`
+
+git checkout $tag
+
 #then commit it to the maven repository and push that to github 
 mvn -DaltDeploymentRepository=repo::default::file:/home/jlong/maven-repository/maven-repository/releases clean deploy

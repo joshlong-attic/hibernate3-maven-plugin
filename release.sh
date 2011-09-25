c34a7f6dd2a541c85cc9032bcbc74183b9cd7f1e
Josh Long <josh.long@springsource.com>
2011-09-24 17:25:54 -0700

    cleanup of the release script.

--- release.sh
+++ release.sh
@@ -6,4 +6,4 @@ tag=`cat release.properties |grep scm.tag|cut -f2 -d=|head -n1`
 git checkout $tag
 
 #then commit it to the maven repository and push that to github 
-mvn -DaltDeploymentRepository=repo::default::file:/home/jlong/maven-repository/maven-repository/releases clean deploy
+mvn -DaltDeploymentRepository=repo::default::file:/home/jlong/maven-repository/releases clean deploy
